67aa57f9b6719dc943374318020cb1ed94904068
mich balkany <askmitchb@gmail.com>
2023-09-13 11:06:30 -0400

    Initial Commit

---
+++ length_converter/length_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  length_converterApp.swift
+//  length_converter
+//
+//  Created by Mich balkany on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct length_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
